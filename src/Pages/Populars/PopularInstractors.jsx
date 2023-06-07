50d57a7931dcda24ce98c45c40107db065f10a23
kabirshuvo <kabirshuvo19@gmail.com>
2023-06-07 19:54:19 +0600

    Instracture info displayed

--- src/Pages/Populars/PopularInstractors.jsx
+++ src/Pages/Populars/PopularInstractors.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
-
+import DisplayInstractors from "../../shared/DisplayInstractors/DisplayInstractors";
 
 const PopularInstructors = () => {
-    return (
-        <section>
-            <SectionTitle title={'our popular instructors'} heading={'Instructors'}></SectionTitle>
-            <h3>Popular instructors: Based on how meny students enrolled on their classes</h3>
-        </section>
-    );
+  const [instructors, setInstructors] = useState([]);
+
+  useEffect(() => {
+    fetch("instructors.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // Sorted the instructors based on the number of students enrolled
+        const popularInstructors = data.sort(
+          (a, b) => b.studentEnrolled - a.studentEnrolled
+        );
+        setInstructors(popularInstructors);
+      });
+  }, []);
+
+  return (
+    <section>
+      <SectionTitle title={"Our Popular Instructors"} heading={"Instructors"} />
+      <h3>Popular instructors: Based on the number of students enrolled in their classes</h3>
+      <div className="p-8">
+        {instructors.map((instructor) => (
+          <DisplayInstractors key={instructor.id} instructor={instructor}>
+            
+          </DisplayInstractors>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularInstructors;
---
+++ src/shared/DisplayInstractors/DisplayInstractors.jsx
@@ -0,0 +1,43 @@
+const DisplayInstructors = ({ instructor }) => {
+  const { name, bio, image, specialization, email, phone, studentEnrolled } =
+    instructor;
+
+  return (
+    <div className="border-accent m-2 p-4 card bg-base-100 shadow-xl">
+      <div className="flex flex-col md:flex-row justify-center items-center">
+        <div className="md:w-1/3">
+          <img
+            className="rounded-lg mb-4"
+            style={{ width: "200px", height: '250px'}}
+            src={image}
+            alt={name}
+          />
+        </div>
+        <div className="md:w-2/3 md:pl-4">
+          <h2 className="text-2xl font-bold">{name}</h2>
+          <p className="text-gray-600 mb-4">{specialization}</p>
+          <p className="mb-4">{bio}</p>
+          <table className="w-full mb-4">
+            <tbody>
+              <tr>
+                <td className="text-gray-600 pr-2">Email:</td>
+                <td>{email}</td>
+              </tr>
+              <tr>
+                <td className="text-gray-600 pr-2">Phone:</td>
+                <td>{phone}</td>
+              </tr>
+              <tr>
+                <td className="text-gray-600 pr-2">Students Enrolled:</td>
+                <td>{studentEnrolled}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button className="btn btn-outline ">Follow</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayInstructors;
