40f41156b24a3704e84386abe13df265c6f6dcee
kabirshuvo <kabirshuvo19@gmail.com>
2023-06-07 23:47:56 +0600

    displaing summer clsses by category

--- src/Pages/AllClasses/AllClasses.jsx
+++ src/Pages/AllClasses/AllClasses.jsx
@@ -1,11 +1,36 @@
-
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import useClases from "../../hooks/useClases";
+import ClassesByCategory from "./ClassesByCategory";
 
 const AllClasses = () => {
-    return (
-        <div>
-            <h3>All Classes: data will be loaded here</h3>
-        </div>
-    );
+  const [classes] = useClases();
+
+  const ecoArtAdventure = classes.filter((cls) => cls.categoryId === 1);
+  const tropicalArtistry = classes.filter((cls) => cls.categoryId === 2);
+  const craftyCritters = classes.filter((cls) => cls.categoryId === 3);
+  const sunnyCreations = classes.filter((cls) => cls.categoryId === 4);
+  const sunShineStudio = classes.filter((cls) => cls.categoryId === 5);
+  const imaginativeExplorations = classes.filter((cls) => cls.categoryId === 6);
+  const paperParadise = classes.filter((cls) => cls.categoryId === 7);
+  
+  return (
+    <div>
+      <h3>All Classes: data will be loaded here</h3>
+      <section className="py-16">
+        <SectionTitle heading='summer Classes' title='Summer classes are good'></SectionTitle>
+      </section>
+
+
+
+<ClassesByCategory cls={ecoArtAdventure}></ClassesByCategory>
+
+
+
+
+
+
+    </div>
+  );
 };
 
 export default AllClasses;
---
+++ src/Pages/AllClasses/ClassesByCategory.jsx
@@ -0,0 +1,18 @@
+import DisplaySummerClasses from "../../shared/DisplaySummerClasses/DisplaySummerClasses";
+
+const ClassesByCategory = ({ cls }) => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-2">
+        {cls.map((classes) => (
+          <DisplaySummerClasses
+            key={classes.id}
+            classes={classes}
+          ></DisplaySummerClasses>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ClassesByCategory;
