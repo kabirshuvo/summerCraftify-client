0d14f3ab8faa9a9f5edc60b45c121e97a4962998
kabirshuvo <kabirshuvo19@gmail.com>
2023-06-11 00:45:11 +0600

    Stripe Publishable Key make via env.local

---
+++ src/Pages/DashBoard/PaYments/CheckOutForm.jsx
@@ -0,0 +1,67 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
+const CheckOutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const [cardError, setCardError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+  };
+
+  return (
+    <>
+      <form className="w-2/3 mx-auto" onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "24px",
+                color: "#336699",
+                "::placeholder": {
+                  color: "#336699",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-outline btn-info btn-sm "
+          type="submit"
+          disabled={!stripe || !clientSecret || processing}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className="text-red-400">{cardError}</p>}
+      {transactionId && (
+        <p className="text-green-400">
+          Transaction complete with TransactionId: {transactionId}
+        </p>
+      )}
+    </>
+  );
+};
+
+export default CheckOutForm;
--- src/Pages/DashBoard/PaYments/PaYments.jsx
+++ src/Pages/DashBoard/PaYments/PaYments.jsx
@@ -1,11 +1,24 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckOutForm from "./CheckOutForm";
 
+// todo:
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const PaYments = () => {
-    return (
-        <div>
-            <h3>PaYments PaYrasia</h3>
-        </div>
-    );
+  return (
+    <div className="w-full my-16">
+      <SectionTitle
+        heading="PaYments PaYrasia"
+        title="Pay more enjoy more"
+      ></SectionTitle>
+
+      <Elements stripe={stripePromise}>
+        <CheckOutForm></CheckOutForm>
+      </Elements>
+    </div>
+  );
 };
 
 export default PaYments;
