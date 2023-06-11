ae39fc9c4f7539a198ebbdf6c2e67135f0f28098
kabirshuvo <kabirshuvo19@gmail.com>
2023-06-12 05:26:10 +0600

    Testing image in readme file

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -6,7 +6,7 @@ import Reviews from '../../Reviews/Reviews';
 import HeroSec from '../HeroSec/HeroSec';
 
 const Home = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
@@ -17,9 +17,9 @@ const Home = () => {
     <div className={`py-16 ${theme === 'dark' ? 'bg-slate-900 text-slate-300' : 'bg-slate-300 text-slate-700'}`}>
       <button
         onClick={toggleTheme}
-        className="fixed top-4 right-4 p-2 rounded-full bg-gray-500 text-white hover:bg-gray-600"
+        className="fixed top-4 right-4 p-2 rounded-full bg-gray-500 text-white hover:bg-gray-600 z-50"
       >
-        Toggle Theme
+        {theme === 'dark' ? 'Light Theme' : 'Dark Theme'}
       </button>
       <section>
         <HeroSec></HeroSec>
